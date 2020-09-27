138ae78bd1135087d58453aba24c86915f36c966
CWMD1500 <71892925+CWMD1500@users.noreply.github.com>
2020-09-27 11:46:28 -0400

    code template provided by the prof.

---
+++ scripts/notes.js
@@ -0,0 +1,27 @@
+let w1 = "W1 Course Intro and DCA Access -topics to be filled by the student";
+
+let w2="W2 Client/Developer Applications -topics to be filled by the student";
+
+let w3="W3 Developer Tools -topics to be filled by the student";
+
+let w4="W4 Developer Repository -topics to be filled by the student";
+
+let w5="W5 Service Provider over the Internet -topics to be filled by the student";
+
+let w6="W6 Web Protocols -topics to be filled by the student";
+
+let w7="W7 Site Publishing -topics to be filled by the student";
+
+let w8="W8 Content Formats -topics to be filled by the student";
+
+let w9="W9 Computer Languages -topics to be filled by the student";
+
+let w10="W10 Language Framework and Library -topics to be filled by the student";
+
+let w11="W11 Developer Collaborations -topics to be filled by the student";
+
+let w12="W12 File Exchanges -topics to be filled by the student";
+
+let w13="W13 Web Tech. Project -topics to be filled by the student";
+
+let w14="W14 Final Exam";
---
+++ scripts/script.js
@@ -0,0 +1,57 @@
+
+document.getElementById("r0c0").onclick = function () {
+   alert(w1);
+};
+
+document.getElementById("r0c1").onclick = function () {
+   alert(w2);
+};
+
+document.getElementById("r0c2").onclick = function () {
+   alert(w3);
+};
+
+document.getElementById("r0c3").onclick = function () {
+   alert(w4);
+};
+
+document.getElementById("r0c4").onclick = function () {
+   alert(w5);
+};
+
+document.getElementById("r1c0").onclick = function () {
+   alert(w6);
+};
+
+document.getElementById("r1c1").onclick = function () {
+   alert(w7);
+};
+
+document.getElementById("r1c2").onclick = function () {
+   alert(w8);
+};
+
+document.getElementById("r1c3").onclick = function () {
+   alert(w9);
+};
+
+document.getElementById("r1c4").onclick = function () {
+   alert(w10);
+};
+
+
+document.getElementById("r2c0").onclick = function () {
+   alert(w11);
+};
+
+document.getElementById("r2c1").onclick = function () {
+   alert(w12);
+};
+
+document.getElementById("r2c2").onclick = function () {
+   alert(w13);
+};
+
+document.getElementById("r2c3").onclick = function () {
+   alert(w14);
+};
